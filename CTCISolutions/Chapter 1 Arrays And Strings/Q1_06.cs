bc35e914c0dafe50e094b2e486afd863c7b1d265
Rahul Srivastava <srivasrk@mail.uc.edu>
2017-07-19 22:06:11 -0400

    Chapter 1  Question 6 - Another solution

--- CTCISolutions/Chapter 1 Arrays And Strings/Q1_06.cs
+++ CTCISolutions/Chapter 1 Arrays And Strings/Q1_06.cs
@@ -36,6 +36,47 @@ namespace CTCISolutions.Chapter_1_Arrays_And_Strings
             }
         }
 
+        private void Rotate2(int[][] matrix, int n)
+        {
+            int t;
+
+            //for eg: start with 
+            //1 2 3
+            //4 5 6
+            //7 8 9
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n / 2; j++)
+                {
+                    t = matrix[i][j];
+                    matrix[i][j] = matrix[i][n - j - 1];
+                    matrix[i][n - j - 1] = t;
+                }
+            }
+            //now we have 
+            //3 2 1 
+            //6 5 4 
+            //9 8 7 
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    t = matrix[i][j];
+                    matrix[i][j] = matrix[n - j - 1][n - i - 1];
+                    matrix[n - j - 1][n - i - 1] = t;
+                }
+            }
+            //now we have 
+            //7 4 1 
+            //8 5 2 
+            //9 6 3 
+
+        }
+
+        /*
+         Rotate an NxN matrix by 90 degrees.
+             */
         public void Run()
         {
             const int size = 3;
@@ -48,6 +89,12 @@ namespace CTCISolutions.Chapter_1_Arrays_And_Strings
 
             Console.WriteLine();
             Console.WriteLine("Rotated Matrix:");
+            AssortedMethods.PrintMatrix(matrix);
+
+            Rotate2(matrix, size);
+
+            Console.WriteLine();
+            Console.WriteLine("Rotated Again Matrix:");
             AssortedMethods.PrintMatrix(matrix);
         }
     }
