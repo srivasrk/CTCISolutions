1b5a181d72f7d2154f73a01ab01e8be0dc0b032c
Rahul Srivastava <srivasrk@mail.uc.edu>
2017-07-19 22:15:44 -0400

    Update question description of Q1_07

--- CTCISolutions/Chapter 1 Arrays And Strings/Q1_07.cs
+++ CTCISolutions/Chapter 1 Arrays And Strings/Q1_07.cs
@@ -47,6 +47,10 @@ namespace CTCISolutions.Chapter_1_Arrays_And_Strings
 
         }
 
+        /*
+         Write an algorithm such that if an element in an MxN matrix is 0, 
+         the entire row and column are set to 0.
+             */
         public void Run()
         {
             const int numberOfRows = 10;
