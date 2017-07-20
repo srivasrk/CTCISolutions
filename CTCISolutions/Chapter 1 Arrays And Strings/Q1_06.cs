b21ee940fdb9b794e2901b3e70a548d5f0283312
Rahul Srivastava <srivasrk@mail.uc.edu>
2017-07-19 21:14:12 -0400

    Added library helper files

---
+++ CTCISolutions/Chapter 1 Arrays And Strings/Q1_06.cs
@@ -0,0 +1,26 @@
+﻿using CTCISolutions.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCISolutions.Chapter_1_Arrays_And_Strings
+{
+    class Q1_06
+    {
+
+        public void Run()
+        {
+            const int size = 3;
+
+            var matrix = AssortedMethods.RandomMatrix(size, size, 0, 9);
+
+            AssortedMethods.PrintMatrix(matrix);
+
+           // Rotate(matrix, size);
+            Console.WriteLine();
+            AssortedMethods.PrintMatrix(matrix);
+        }
+    }
+}
