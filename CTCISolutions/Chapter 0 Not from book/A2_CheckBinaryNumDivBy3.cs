1111a384590cd8cc54d08289c6b312523156cc9e
Rahul Srivastava <rahul.srivastava@sigmatek.net>
2017-08-30 16:08:56 -0400

    Check Binary number divisible by 3

---
+++ CTCISolutions/Chapter 0 Not from book/A2_CheckBinaryNumDivBy3.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCISolutions.Chapter_0_Not_from_book
+{
+    class A2_CheckBinaryNumDivBy3
+    {
+        // Explanataion: 
+        // A binary number is a multiple of 3 if and only if the alternating sum 
+        // of its bits is also a multiple of 3:
+        // 4   = 100       1 - 0 + 0 = 1, not multiple of 3
+        // 6   = 110       1 - 1 + 0 = 0, multiple of 3
+        // 78  = 1001110   1 - 0 + 0 - 1 + 1 - 1 + 0 = 0, multiple of 3
+        // 109 = 1101101   1 - 1 + 0 - 1 + 1 - 0 + 1 = 1, not multiple of 3
+        private bool IsDivisibleBy3(int[] input)
+        {
+            int total = 0;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    total = total + input[i];
+                }
+                else
+                {
+                    total = total - input[i];
+                }
+            }
+
+            if ((total % 3) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Run()
+        {
+            int[] num1 = new int[] { 1, 1, 0, 1, 1, 0, 1 };
+            Console.WriteLine("Num1 : 109");
+            for (int i = 0; i < num1.Length; i++)
+            {
+                Console.Write(num1[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(IsDivisibleBy3(num1));
+            Console.WriteLine();
+
+            int[] num2 = new int[] { 1, 1, 0 };
+            Console.WriteLine("Num2 : 6");
+            for (int i = 0; i < num2.Length; i++)
+            {
+                Console.Write(num2[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(IsDivisibleBy3(num2));
+
+            Console.ReadLine();
+        }
+    }
+}
--- CTCISolutions/Program.cs
+++ CTCISolutions/Program.cs
@@ -13,7 +13,8 @@ namespace CTCISolutions
     {
         static void Main(string[] args)
         {
-            A1_QuickSort solution = new A1_QuickSort();
+            //A1_QuickSort solution = new A1_QuickSort();
+            A2_CheckBinaryNumDivBy3 solution = new A2_CheckBinaryNumDivBy3();
             //Q1_01 solution = new Q1_01();
             //Q1_03 solution = new Q1_03();
             //Q1_04 solution = new Q1_04();
